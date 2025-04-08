ed941e11737c6efdf923c39ce5e622d2f99af691
Deyson Venté <deiandresvent@hotmail.com>
2025-04-07 19:36:58 -0500

    Agregar configuración de Swagger y validación global en el bootstrap de la aplicación

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Config } from 'config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configSwagger = new DocumentBuilder()
+    .setTitle('App Comentarios')
+    .setDescription(
+      'API - Backend app de comentarios en NESTJS y SQL Server - 2025',
+    )
+    .setVersion('1.0')
+    .addTag('Comentario', 'Controlador - EndPoints de Comentarios')
+    .addTag('Publicacion', 'Controlador - EndPoints de Publicaciones')
+    .build();
+  app.enableCors();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('swagger/index.html', app, documentFactory, {
+    explorer: true,
+    jsonDocumentUrl: 'swagger/json',
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 
   console.log(`Server ${3000}:, ${await app.getUrl()}`);
